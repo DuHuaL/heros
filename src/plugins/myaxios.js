02043a30852c55b13a6d0a9f92678fbd5311068a
DuHuaL <469580396@qq.com>
2020-09-03 15:46:47 +0800

    完善axios的封装

--- src/plugins/myaxios.js
+++ src/plugins/myaxios.js
@@ -2,9 +2,18 @@ import axios from 'axios';
 
 const MyAxios = {};
 MyAxios.install = function(Vue) {
-  // 全局设置axios的baseURL
-  axios.defaults.baseURL = 'http://localhost:3000';
-  Vue.prototype.$http = axios;
+  // 全局设置axios的baseURL，基地址只能有一个
+  // axios.defaults.baseURL = 'http://localhost:3000';
+
+  // 由于可能有不同的请求地址，所以此方法可以设置不同的基地址
+  const instance = axios.create({
+    baseURL: 'http://localhost:3000'
+  });
+  // const instance1 = axios.create({
+  //   baseURL = 'http://localhost:8080'
+  // });
+  Vue.prototype.$http = instance;
+  // Vue.prototype.$http1 = instance1;
 };
 
 export default MyAxios;
