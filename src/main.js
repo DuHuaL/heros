5ea975d1d4fb371e4ef6684f0cd432a0407484e5
DuHuaL <469580396@qq.com>
2020-09-03 15:22:40 +0800

    axios的简单封装

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
-import axios from 'axios';
+// 原始写法
+// import axios from 'axios';
+// 插件使用
+import MyAxios from '@/plugins/myaxios';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/css/index.css';
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
-Vue.prototype.$http = axios;
+// 原始写法
+// Vue.prototype.$http = axios;
+Vue.use(MyAxios);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
